1c6177d0b32fe697b51bd75179226f50ab7eb8d7
Dmitry Durach <dmitry@onix-systems.com>
2017-11-27 22:03:07 +0200

    Initital commit

---
+++ LedPanel.h
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+#define LED_PANEL_WIDTH 4
+#define LED_PANEL_HEIGHT 1
+#define LED_PANEL_PIN_CS 10
+#define LED_PANEL_BRIGHTNESS 2
+
+class LedPanel {
+
+  private:
+    Max72xxPanel _matrix = Max72xxPanel(LED_PANEL_PIN_CS, LED_PANEL_WIDTH, LED_PANEL_HEIGHT); 
+    String xxx = "xxxxxxxxxx"; // some code overwrites bytes in the string here #3 and #7
+    
+  public:
+    LedPanel();
+    void setup();  
+    void test();
+};
+
