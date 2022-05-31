df6b7714d9fa6a912e068306866f12724d528f41
Dmitry Durach <979567+durach@users.noreply.github.com>
2022-05-31 09:44:06 +0300

    Introducing PCINT

--- config.h
+++ config.h
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 
-#define MAX_TABLES 4
+#define MAX_TABLES 6
 #define START_LAMP_DURATION 1 // seconds
 
-#define PIN_BUTTON_TABLE_0 19
-#define PIN_BUTTON_TABLE_1 21
-#define PIN_BUTTON_TABLE_2 20
-#define PIN_BUTTON_TABLE_3 18
+#define PIN_BUTTON_TABLE_0 53
+#define PIN_BUTTON_TABLE_1 52
+#define PIN_BUTTON_TABLE_2 51
+#define PIN_BUTTON_TABLE_3 50
+#define PIN_BUTTON_TABLE_4 10
+#define PIN_BUTTON_TABLE_5 11
 
 #define PIN_BUTTON_START_60 42
 #define PIN_BUTTON_START_20 40
@@ -16,9 +18,12 @@
 #define PIN_BUZZER 25
 
 #define PIN_LED_TABLE_0 3
-#define PIN_LED_TABLE_1 5
-#define PIN_LED_TABLE_2 7
-#define PIN_LED_TABLE_3 9
+#define PIN_LED_TABLE_1 4
+#define PIN_LED_TABLE_2 5
+#define PIN_LED_TABLE_3 6
+#define PIN_LED_TABLE_4 7
+#define PIN_LED_TABLE_5 8
+
 #define PIN_LED_START 15
 
 #define PIN_PANEL_CLK 44
@@ -37,9 +42,11 @@
 #define TABLE_1 1
 #define TABLE_2 2
 #define TABLE_3 3
+#define TABLE_4 4
+#define TABLE_5 5
 
 //#define START_SCREEN_TIME 1.00
 
 #define ERROR_NOT_INITED 0
 #define ERROR_UNKNOWN_STATE 1
-#define ERROR_BUTTONS_COLLISION 2
+#define ERROR_BUTTONS 2
