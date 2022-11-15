75a5bd6db562ebde1eff4e70bd6f95731d02df7d
Dmitry Durach <dmitry@onix-systems.com>
2022-11-15 20:11:55 +0200

    Adding finishing

--- Buzzer.h
+++ Buzzer.h
@@ -3,6 +3,7 @@
 #define BUZZER_FREQUENCY_START 800 // Hz
 #define BUZZER_FREQUENCY_TABLE 700 // Hz
 #define BUZZER_FREQUENCY_FALSE_START 600 // Hz
+#define BUZZER_FREQUENCY_FINISHING 600 // Hz
 #define BUZZER_FREQUENCY_FINISH 550 // Hz
 
 #define BUZZER_SOUND_DURATION 1000 // ms
@@ -18,6 +19,7 @@ class Buzzer {
     void playStartSound();
     void playTableSound();
     void playFalseStartSound();
+    void playFinishingSound();
     void playFinishSound();
     void off();
 };
--- Timer.cpp
+++ Timer.cpp
@@ -32,10 +32,13 @@ void Timer::tick() {
     isUpdated = true;
   }
   
-  if ((value - _oldValue) >= TIMER_PRECISION) {
+  uint32_t precision = is_finishing() ? TIMER_PRECISION_FINISHING : TIMER_PRECISION;
+
+  if ((value - _oldValue) >= precision) {
     _oldValue = value;
     isUpdated = true;
   }
+
 }
 
 void Timer::reset() {
@@ -44,4 +47,9 @@ void Timer::reset() {
   isRunning = false;
   isFinished = false;
   isUpdated = true;
+}
+
+bool Timer::is_finishing() {
+  uint32_t finishing_value = millis() - _startTime;
+  return (_target - finishing_value) <= IS_FINISHING;
 }
